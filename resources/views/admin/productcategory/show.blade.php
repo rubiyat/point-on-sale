f558851833f93c37a47dc84e150363de3fda4193
Nahin Munker <nmunk1@ocdsb.ca>
2017-10-20 20:34:38 -0400

    Product Category show view #9

---
+++ resources/views/admin/productcategory/show.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.admin')
+
+@section('content')
+
+	<h1>Product Category</h1><h2 style="margin-left: 800px; margin-bottom: 20px;"><a href="">Admin</a> / <a href="">Product Category</a> / Show</h2>
+
+	<hr style="border-top: solid #DCDCDC 1px;">
+
+	<button class="btn btn-primary" style="margin-bottom: 20px;">New Product Category</button>
+	
+	<table class="table table-bordered table-hover">
+	    <thead class="thead-inverse">
+<!-- 	      <tr>
+	        <th>SL</th>
+	        <th>Name</th>
+	        <th>Description</th>
+	        <th>Action</th>
+	      </tr> -->
+	    </thead>
+	    <tbody>
+	      <tr>
+	        <th scope="row">Category Name:</th>
+	        <td>Test</td>
+	      </tr>
+	      <tr>
+	        <th scope="row">Description:</th>
+	        <td>Some text for description</td>	
+	      </tr>
+	      <tr>
+	        <th scope="row">Status:</th>
+	        <td>Published</td>
+	      </tr>
+	    </tbody>
+	</table>
+
+
+	<div style="margin-right:auto; margin-left:500px;">
+		<button class="btn btn-warning">&larr; Back</button>
+	</div>
+
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
- Route::get('/admin', function () {
-        return view('admin.index');
-    });
+Route::get('/admin', function () {
+    return view('admin.index');
+});
+
+Route::get('/admin/productcategory', function () {
+    return view('admin.productcategory.index');
+});
+
+Route::get('/admin/productcategory/show', function () {
+    return view('admin.productcategory.show');
+});
