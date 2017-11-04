5b863eeb76f9f181f89d6d1d019649910a0fcc28
Zahin Abrer <zahin212@outlook.com>
2017-11-04 12:56:32 -0400

    Product Category edit view #8

--- resources/views/admin/productcategory/index.blade.php
+++ resources/views/admin/productcategory/index.blade.php
@@ -6,50 +6,48 @@
 
 	<hr style="border-top: solid #DCDCDC 1px;">
 
-	<!-- <input type="button" name="buttonNewCategory" style="margin-bottom: 20px;" class="btn btn-primary" value="New Product Category">
- -->
- <a class="btn btn-primary" href="{{ route('admin.productcategory.create')}}">New Product Category </a>
+    <a class="btn btn-primary" href="{{route('productcategory.create')}}">New Product Category </a>
 
-            <table class="table table-bordered table-hover table-responsive">
-                <thead>
+    <table class="table table-bordered table-hover table-responsive">
+        <thead>
+        <tr>
+            <th>SL</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Action</th>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @if($productCategories)
+
+            @foreach($productCategories as $productCategory)
+
                 <tr>
-            	  <th>SL</th>
-	        	  <th>Name</th>
-	        	  <th>Description</th>
-	        	  <th>Action</th>
-	        	  <th></th>
-                </tr>
-                </thead>
-                <tbody>
 
-					@if($productCategories) 
-
-                    	@foreach($productCategories as $productCategory)
-
-                    	<tr>
-
-                        <td>{{$productCategory->id}}</td>
-                       	<td>{{$productCategory->name}}</td>
-                        <td>{{$productCategory->description}}</td>
-                        <td>{{$productCategory->status}}</td>
+                    <td>{{$productCategory->id}}</td>
+                    <td>{{$productCategory->name}}</td>
+                    <td>{{$productCategory->description}}</td>
+                    <td>{{$productCategory->status}}</td>
 
                        
-                        <td>
-                        	<a href="{{route('admin.productcategory.show', $productCategory->id)}}" class="btn btn-warning"><i class="icon-eye-open" ></i><span class="hidden-tablet"> Show</span></a>
+                    <td>
+                        <a href="{{route('productcategory.show', $productCategory->id)}}" class="btn btn-warning"><i class="icon-eye-open" ></i><span class="hidden-tablet"> Show</span></a>
 
-                        	<a class="btn btn-info" href="{{url('/admin/productcategory/edit')}}"><i class="icon-edit"></i><span class="hidden-tablet"> Edit</span></a>
+                        <a class="btn btn-info" href="{{route('productcategory.edit', $productCategory->id)}}"><i class="icon-edit"></i><span class="hidden-tablet"> Edit</span></a>
 
-                        	<a href="#" class="btn btn-danger"><i class="icon-trash"></i><span class="hidden-tablet"> Delete</span></a>
+                        <a href="#" class="btn btn-danger"><i class="icon-trash"></i><span class="hidden-tablet"> Delete</span></a>
 
-                        </td>
+                    </td>
 
-						</tr>
+                </tr>
 
-	        			@endforeach
+            @endforeach
 
-	        		@endif
+        @endif
 
-                </tbody>
-            </table>
+        </tbody>
+    </table>
 
 @stop
