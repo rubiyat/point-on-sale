8aec8f1a3a982ea94940002714a913cbb63e31c6
Sharif Mohammad <sharifmohammad017@gmail.com>
2017-12-02 18:08:57 -0800

    Product Model #43

--- app/Product.php
+++ app/Product.php
@@ -6,14 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-   // protected $fillable = [
-   //  	'id',
-   //  	'name',
-   //  	'description',
-   //  	'price',
-   //  	'image',
-   //  	'vate_rate',
-   //  	'product_category_id',
-   //  	'status'
-   //  ];
+   protected $fillable = [
+    	'id',
+    	'name',
+    	'description',
+    	'price',
+    	'image',
+    	'vate_rate',
+    	'product_category_id',
+    	'status'
+    ];
 }
