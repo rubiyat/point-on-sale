b9bfbbad27c7bdadbe913c2449563c30c2d5a5b1
rubiyat <rubiyat21@gmail.com>
2017-12-18 15:45:31 +0600

    Customer Table #34

---
+++ database/migrations/2017_12_18_090704_create_customers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);
+            $table->string('address');
+            $table->string('phone_number', 100);
+            $table->char('gender');
+            $table->string('email', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
