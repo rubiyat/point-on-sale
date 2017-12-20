31a893ba97106c2aeb3a2c64775b14a06adfd725
rubiyat <rubiyat21@gmail.com>
2017-12-20 13:39:33 +0600

    Employee Model #51

---
+++ app/Employee.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+     protected $fillable = [
+
+    	'id',
+    	'first_name',
+    	'last_name',
+    	'address',
+    	'phone',
+    	'gender',
+    	'email',
+    	'image',
+    	'date_of_birth',
+    	'hire_date',
+    	'role_id'
+
+    ];
+}
