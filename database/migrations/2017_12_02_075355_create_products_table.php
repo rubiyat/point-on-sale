1597ca948b63cbdb301bfd4b13667d610f089a83
rubiyat <rubiyat21@gmail.com>
2018-01-11 13:55:31 +0600

    Product Table #42

--- database/migrations/2017_12_02_075355_create_products_table.php
+++ database/migrations/2017_12_02_075355_create_products_table.php
@@ -15,13 +15,15 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->text('description');
+            $table->string('name', 100);
+            $table->text('description')->nullable();
             $table->float('price');
-            $table->string('image');
-            $table->float('vate_rate');
-        
-            $table->char('status');
+            $table->string('image')->nullable();
+            $table->float('vat_rate');
+            $table->integer('product_category_id')->unsigned();
+            $table->boolean('status')->default(1);
+
+            $table->foreign('product_category_id')->references('id')->on('product_categories')->onDelete('restrict')->onUpdate('cascade');
             $table->timestamps();
         });
     }
