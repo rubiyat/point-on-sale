d0b5d0f4de03271cd60055ac4163d55033419f58
rubiyat <rubiyat21@gmail.com>
2018-01-15 14:42:29 +0600

    Sale Table #79

---
+++ database/migrations/2018_01_15_083649_create_sales_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('customer_id')->unsigned()->index();
+
+            $table->float('total_sale_price');
+            $table->integer('total_sale_quantity');
+            $table->date('sale_date_time');
+
+            $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('restrict')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
